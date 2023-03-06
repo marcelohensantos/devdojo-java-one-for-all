a947fbbdcb70c6690186dffa4cb7e8b476bf6c24
Marcelo Henrique dos Santos <hs.marcelo@outlook.com>
2023-03-06 12:22:00 +0000

    source code class 125

--- src/academy/devdojo/javaoneforall/javacore/Rdates/test/PeriodTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Rdates/test/PeriodTest01.java
@@ -2,6 +2,7 @@ package academy.devdojo.javaoneforall.javacore.Rdates.test;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
@@ -17,5 +18,6 @@ public class PeriodTest01 {
         System.out.println(p2);
         System.out.println(p3);
         System.out.println(p4);
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.YEARS));
     }
 }
